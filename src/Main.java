a478932534e5fb749979ad5e051b5bf9cc7c28e9
GeekVan <lychallengers@gmail.com>
2020-11-12 10:29:55 +0800

    奇偶排序数组II

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,22 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+//922. 按奇偶排序数组 II
 
 class Main {
-    public int[][] kClosest(int[][] points, int K) {
-        Arrays.sort(points, Comparator.comparingInt(a->a[0]*a[0]+a[1]*a[1]));
-        return Arrays.copyOf(points,K);
+
+    public int[] sortArrayByParityII(int[] A) {
+        int i=0;
+        int j=1;
+        while(i<A.length&&j<A.length){
+            while(i<A.length&&A[i]%2==0)i=i+2;
+            while(j<A.length&&A[j]%2==1)j=j+2;
+            if(i<A.length&&j<A.length){
+                int tmp=A[i];
+                A[i]=A[j];
+                A[j]=tmp;
+            }
+        }
+        return A;
     }
 }
