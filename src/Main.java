88f26217cd0e693ef63c6e4a5e5d3939e09bd424
GeekVan <lychallengers@gmail.com>
2020-11-16 10:09:22 +0800

    406.根据身高重建队列

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,57 @@
 import java.lang.*;
 import java.util.*;
 
-//402.移除k位数字
+//406. 根据身高重建队列
 
+class man{
+    int h;
+    int k;
+    man(int h,int k){
+        this.h=h;
+        this.k=k;
+    }
+}
 class Main {
 
-    public String removeKdigits(String num, int k) {
-        if(num.length()==k){
-            return "0";
+    public int[][] reconstructQueue(int[][] people) {
+        ArrayList<man> arrayList=new ArrayList<>();
+        for(int i=0;i<people.length;i++){
+            arrayList.add(new man(people[i][0],people[i][1]));
         }
-        int i,j;
-        for(i=0;i<k;i++){
-            boolean tag=false;
-            for(j=0;j<num.length()-1;j++){
-                if(num.charAt(j)> num.charAt(j+1)){
-                    num=num.substring(0,j)+num.substring(j+1);
-                    tag=true;
-                    break;
-                }
-            }
-            if(!tag){
-                num=num.substring(0,num.length()-1);
-            }
-            int off=0;
-            for(j=0;j<num.length();j++){
-                if(num.charAt(j)=='0'){
-                    off++;
-                }
-                else{
-                    break;
-                }
-            }
-            num=num.substring(off);
-        }
-        int off=0;
-        for(j=0;j<num.length();j++){
-            if(num.charAt(j)=='0'){
-                off++;
+        arrayList.sort((o1,o2)->{
+            if(o1.h!=o2.h){
+                return o1.h-o2.h;
             }
             else{
-                break;
+                return o2.k-o1.k;
             }
+        });
+        boolean []visit=new boolean[people.length];
+        for(int i=0;i<arrayList.size();i++){
+            int h=arrayList.get(i).h;
+            int k=arrayList.get(i).k;
+            int off=0;
+            for(int j=0;j<visit.length;j++){
+                if(visit[j]==false){
+                    if(off==k){
+                        people[j][0]=h;
+                        people[j][1]=k;
+                        visit[j]=true;
+                        break;
+                    }
+                    off++;
+                }
+
+            }
+
         }
-        num=num.substring(off);
-        if(num.length()==0){
-            return "0";
-        }
-        else{
-            return num;
-        }
+        return people;
+
+    }
+
+    public static void main(String[] args) {
+        int [][]arr=new int[][]{{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+        new Main().reconstructQueue(arr);
     }
 
 }
