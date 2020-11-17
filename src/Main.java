1cbf1e263b27078e5683cee07eeff36e7833e51a
GeekVan <lychallengers@gmail.com>
2020-11-17 09:46:16 +0800

    1030. 距离顺序排列矩阵单元格

--- src/Main.java
+++ src/Main.java
@@ -2,49 +2,37 @@
 import java.lang.*;
 import java.util.*;
 
-//406. 根据身高重建队列
+//1030. 距离顺序排列矩阵单元格
 
-class man{
-    int h;
-    int k;
-    man(int h,int k){
-        this.h=h;
-        this.k=k;
+class mat{
+    int [] location;
+    int dis;
+    mat(int []location,int dis){
+        this.location=location;
+        this.dis=dis;
     }
 }
 class Main {
 
-    public int[][] reconstructQueue(int[][] people) {
-        ArrayList<man> arrayList=new ArrayList<>();
-        for(int i=0;i<people.length;i++){
-            arrayList.add(new man(people[i][0],people[i][1]));
-        }
-        arrayList.sort((o1,o2)->{
-            if(o1.h!=o2.h){
-                return o1.h-o2.h;
-            }
-            else{
-                return o2.k-o1.k;
-            }
-        });
-       ArrayList<man> result=new ArrayList<>();
-        for(int i=arrayList.size()-1;i>=0;i--){
-            result.add(arrayList.get(i).k,arrayList.get(i));
-        }
-
-        for(int i=0;i<result.size();i++){
-            int h=result.get(i).h;
-            int k=result.get(i).k;
-            people[i][0]=h;
-            people[i][1]=k;
-        }
-        return people;
-
+    public int getDis(int x,int y,int i,int j){
+        return Math.abs(x-i)+Math.abs(y-j);
     }
-
-    public static void main(String[] args) {
-        int [][]arr=new int[][]{{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
-        new Main().reconstructQueue(arr);
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+       ArrayList<mat> arrayList=new ArrayList<>();
+       for(int i=0;i<R;i++){
+           for(int j=0;j<C;j++){
+               int dis=getDis(i,j,r0,c0);
+               arrayList.add(new mat(new int[]{i,j},dis));
+           }
+       }
+       arrayList.sort((o1,o2)->{
+           return o1.dis-o2.dis;
+       });
+       int [][] result=new int[arrayList.size()][2];
+       for(int i=0;i<arrayList.size();i++){
+           result[i]=arrayList.get(i).location;
+       }
+       return result;
     }
 
 }
