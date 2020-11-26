94663fd5c7f3a99a74f61ef2b43eaec6784489b7
GeekVan <lychallengers@gmail.com>
2020-11-26 09:41:56 +0800

    164. 最大间距

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,19 @@
 import java.lang.*;
 import java.util.*;
 
-//1370. 上升下降字符串
+//164. 最大间距
 
 
 
 class Main {
 
-    public String sortString(String s) {
-        String table="abcdefghijklmnopqrstuvwxyz";
-        HashMap<Character,Integer> hashMap=new HashMap<>();
-        for(int i=0;i<table.length();i++){
-            hashMap.put(table.charAt(i),0);
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        int max_dis=0;
+        for(int i=0;i<nums.length-1;i++){
+            max_dis=Integer.max(max_dis,nums[i+1]-nums[i]);
         }
-        for(int i=0;i<s.length();i++){
-            hashMap.put(s.charAt(i),hashMap.getOrDefault(s.charAt(i),0)+1);
-        }
-        int count=0;
-        int tag=-1;
-        StringBuffer result=new StringBuffer();
-        while(count<s.length()){
-            int ct=0;
-            if(tag==-1){//正向
-                for(int i=0;i<table.length();i++){
-                    if(hashMap.get(table.charAt(i))!=0){
-                        ct++;
-                        result.append(table.charAt(i));
-                        hashMap.put(table.charAt(i),hashMap.get(table.charAt(i))-1);
-                    }
-                }
-            }
-            else{//反向
-                for(int i=table.length()-1;i>=0;i--){
-                    if(hashMap.get(table.charAt(i))!=0){
-                        ct++;
-                        result.append(table.charAt(i));
-                        hashMap.put(table.charAt(i),hashMap.get(table.charAt(i))-1);
-                    }
-                }
-            }
-            count=count+ct;
-            tag=tag*(-1);
-        }
-        return  result.toString();
+        return max_dis;
     }
 
 }
