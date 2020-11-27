bbf118b6e92c905b90657fad5e98ead59421e8fe
GeekVan <lychallengers@gmail.com>
2020-11-27 14:42:48 +0800

    454. 四数相加 II

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,29 @@
 import java.lang.*;
 import java.util.*;
 
-//164. 最大间距
+//454. 四数相加 II
 
 
 
 class Main {
 
-    public int maximumGap(int[] nums) {
-        Arrays.sort(nums);
-        int max_dis=0;
-        for(int i=0;i<nums.length-1;i++){
-            max_dis=Integer.max(max_dis,nums[i+1]-nums[i]);
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        for(int i=0;i<A.length;i++){
+            for(int j=0;j<B.length;j++){
+                hashMap.put(A[i]+B[j],hashMap.getOrDefault(A[i]+B[j],0)+1);
+            }
         }
-        return max_dis;
+        int count=0;
+        for(int i=0;i<C.length;i++){
+            for(int j=0;j<D.length;j++){
+                int t=-(C[i]+D[j]);
+                if(hashMap.containsKey(t)){
+                   count=count+hashMap.get(t);
+                }
+            }
+        }
+        return count;
     }
 
 }
