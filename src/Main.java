1c657e31f3e5c300a17e917ae3f1e09c99b6dc3b
GeekVan <lychallengers@gmail.com>
2020-11-29 09:01:58 +0800

    976. 三角形的最大周长

--- src/Main.java
+++ src/Main.java
@@ -4,63 +4,21 @@ import org.omg.PortableInterceptor.INACTIVE;
 import java.lang.*;
 import java.util.*;
 
-//493. 翻转对
+//976. 三角形的最大周长
 
 
 
 class Main {
 
-    public  int binary(List<Integer> list,int k){//upper bound;
-        int left=0;
-        int right=list.size()-1;
-        while(left<right){
-            int mid=left+(right-left+1)/2;
-            if(list.get(mid)<=k){
-                left=mid;
-            }
-            else{
-                right=mid-1;
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        int max=0;
+        for(int i=0;i<A.length-2;i++){
+            if(A[i]+A[i+1]>A[i+2]){
+                max=Math.max(max,A[i]+A[i+1]+A[i+2]);
             }
         }
-        return  left;
-    }
-    public int reversePairs(int[] nums) {
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            list.add(nums[i]);
-        }
-        list.sort(Comparator.comparingInt(o -> o));
-        int count=0;
-        int target;
-        for(int i=0;i<nums.length;i++){
-            int t=nums[i];
-            int id=binary(list,t);//search data
-            //remove data
-            list.remove(id);
-            //search target;
-            if(t>=0){
-                if(t%2==0){
-                    target=t/2-1;
-                }
-                else{
-                    target=t/2;
-                }
-            }
-            else{
-                if((-t)%2==0){
-                    target=t/2-1;
-                }
-                else{
-                    target=t/2-1;
-                }
-            }
-
-            int off=binary(list,target);
-            if(off<list.size()&&list.get(off)<=target){
-                count=count+off+1;
-            }
-        }
-        return  count;
+        return  max;
     }
 
 }
