70410d569432bf6b06c3c0113740ac3802dd9a1b
GeekVan <lychallengers@gmail.com>
2020-11-30 19:11:29 +0800

    nowcoder 最大和子矩阵

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,41 @@ import org.omg.PortableInterceptor.INACTIVE;
 import java.lang.*;
 import java.util.*;
 
-//1672. 最富有客户的资产总量
+//nowcoder 最大和子矩阵
 
 
 
 
 class Main {
 
-    public int maximumWealth(int[][] accounts) {
-        int max=1;
-        for(int i=0;i<accounts.length;i++){
-            int sum=0;
-            for(int j=0;j<accounts[0].length;j++){
-                sum=sum+accounts[i][j];
-            }
-            max=Math.max(sum,max);
+    public  int getMax(int []tmp){
+        int max=tmp[0];
+        int res=tmp[0];
+        for(int i=1;i< tmp.length;i++){
+            max=Math.max(tmp[i]+max,tmp[i]);
+            res=Math.max(res,max);
         }
-        return  max;
+        return  res;
+    }
+    public int sumOfSubMatrix(int[][] mat, int n) {
+        // write code here
+        int [][] prefix=new int[mat.length+1][mat[0].length];
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                prefix[i+1][j]=prefix[i][j]+mat[i][j];
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<prefix.length;i++){
+            for(int j=i+1;j<prefix.length;j++){
+                int []tmp=new int[mat[0].length];
+                for(int k=0;k<mat[0].length;k++){
+                    tmp[k]=prefix[j][k]-prefix[i][k];
+                }
+                max=Math.max(max,getMax(tmp));
+            }
+        }
+        return max;
     }
 
 }
