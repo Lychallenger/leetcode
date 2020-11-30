05e4d92b679930d1b074ee8c53af65a45a45aac3
GeekVan <lychallengers@gmail.com>
2020-11-30 19:35:56 +0800

    nowcoder 魔术索引I

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,30 @@ import org.omg.PortableInterceptor.INACTIVE;
 import java.lang.*;
 import java.util.*;
 
-//nowcoder 机器人走方格II
+//nowcoder 魔术索引I
 
 
 
 
 class Main {
 
-    public int countWays(int[][] map, int x, int y) {
+    public boolean findMagicIndex(int[] A, int n) {
         // write code here
-        int [][]dp=new int[map.length][map[0].length];
-        for(int j=0;j<y;j++){
-            if(map[0][j]==1){
-                dp[0][j]=1;
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            int mid=left+(right-left+1)/2;
+            if(A[mid]>mid){
+                right=mid-1;
+            }
+            else if(A[mid]<mid){
+                left=mid+1;
             }
             else{
-                break;
+                return true;
             }
         }
-        for(int i=0;i<x;i++){
-            if(map[i][0]==1){
-                dp[i][0]=1;
-            }
-            else{
-                break;
-            }
-        }
-        for(int i=1;i<x;i++){
-            for(int j=1;j<y;j++){
-                if(map[i][j]!=1){
-                    dp[i][j]=0;
-                }
-                else{
-                    dp[i][j]=(dp[i-1][j]+dp[i][j-1])%1000000007;
-                }
-            }
-        }
-        return dp[dp.length-1][dp[0].length-1];
+        return false;
     }
 
 }
