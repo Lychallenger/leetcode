ac8b42aea2961acec2f6a60cde3828d44fa7011d
GeekVan <lychallengers@gmail.com>
2020-11-30 10:30:10 +0800

    767. 重构字符串

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,52 @@
 
+import com.sun.org.apache.regexp.internal.RE;
 import org.omg.PortableInterceptor.INACTIVE;
 
 import java.lang.*;
 import java.util.*;
 
-//976. 三角形的最大周长
+//767. 重构字符串
 
 
+class letter{
+    char ch;
+    int count;
+    letter(char ch,int count){
+        this.ch=ch;
+        this.count=count;
+    }
+}
 
 class Main {
 
-    public int largestPerimeter(int[] A) {
-        Arrays.sort(A);
-        int max=0;
-        for(int i=0;i<A.length-2;i++){
-            if(A[i]+A[i+1]>A[i+2]){
-                max=Math.max(max,A[i]+A[i+1]+A[i+2]);
-            }
+    public String reorganizeString(String S) {
+        PriorityQueue<letter> priorityQueue=new PriorityQueue<>(26,((o1, o2) ->{
+            return o2.count-o1.count;
+        }));
+        HashMap<Character,Integer> hashMap=new HashMap<>();
+        for(int i=0;i<S.length();i++){
+            hashMap.put(S.charAt(i),hashMap.getOrDefault(S.charAt(i),0)+1);
         }
-        return  max;
+        hashMap.forEach((k,v)->{
+            priorityQueue.add(new letter(k,v));
+        });
+        StringBuffer sb=new StringBuffer();
+        while(priorityQueue.size()>1){
+            letter l1=priorityQueue.poll();
+            letter l2=priorityQueue.poll();
+            l1.count--;
+            if(l1.count>0)priorityQueue.add(l1);
+            l2.count--;
+            if(l2.count>0)priorityQueue.add(l2);
+            sb.append(l1.ch);
+            sb.append(l2.ch);
+        }
+        if(priorityQueue.size()!=0){
+            letter l=priorityQueue.poll();
+            if(l.count!=1)return "";
+            sb.append(l.ch);
+        }
+        return sb.toString();
     }
 
 }
