0ee71e289e6c0ac3250de495b54cf10bf7ab5be4
GeekVan <lychallengers@gmail.com>
2020-12-07 09:47:53 +0800

    861. 翻转矩阵后的得分

--- src/Main.java
+++ src/Main.java
@@ -6,45 +6,24 @@ import java.io.FileReader;
 import java.lang.*;
 import java.util.*;
 
-//659. 分割数组为连续子序列
+//861. 翻转矩阵后的得分
 
 
 
 
 class Main {
 
-    public boolean isPossible(int[] nums) {
-        HashMap<Integer,Integer> hashMap=new HashMap<>();
-        HashMap<Integer,Integer> endHashMap=new HashMap<>();
-        for(int t:nums){
-            hashMap.put(t,hashMap.getOrDefault(t,0)+1);
-        }
-        for(int t:nums){
-            int count=hashMap.getOrDefault(t,0);
-            if(count>0){
-                int pre=endHashMap.getOrDefault(t-1,0);
-                if(pre>0){
-                    hashMap.put(t,count-1);
-                    endHashMap.put(t-1,pre-1);
-                    endHashMap.put(t,endHashMap.getOrDefault(t,0)+1);
-                }
-                else{
-                    int c1=hashMap.getOrDefault(t+1,0);
-                    int c2=hashMap.getOrDefault(t+2,0);
-                    if(c1>0&&c2>0){
-                        hashMap.put(t,count-1);
-                        hashMap.put(t+1,c1-1);
-                        hashMap.put(t+2,c2-1);
-                        endHashMap.put(t+2,endHashMap.getOrDefault(t+2,0)+1);
-                    }
-                    else{
-                        return false;
-                    }
-                }
+    public int matrixScore(int[][] A) {
+        int ans=(1<<(A[0].length-1))*A.length;
+        for(int j=1;j<A[0].length;j++){
+            int ct=0;
+            for(int i=0;i<A.length;i++){
+                if(A[i][j]==A[i][0])ct++;
             }
-
+            int max=Math.max(ct,A.length-ct);
+            ans=ans+(1<<(A[0].length-1))*max;
         }
-        return true;
+        return ans;
     }
 
 
