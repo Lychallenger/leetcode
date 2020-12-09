4732d23cfe6c094a632bd4b414b0fce9dc6dbcf6
GeekVan <lychallengers@gmail.com>
2020-12-09 09:37:30 +0800

    62. 不同路径

--- src/Main.java
+++ src/Main.java
@@ -6,90 +6,26 @@ import java.io.FileReader;
 import java.lang.*;
 import java.util.*;
 
-//842. 将数组拆分成斐波那契序列
-
+//62. 不同路径
 
 
 
 class Main {
-    public boolean tag=false;
-    public List<Integer> result=new ArrayList<>();
-    public boolean isSplit(String s){
-        if(s.length()>1&&s.charAt(0)=='0'){
-            return false;
+    public int uniquePaths(int m, int n) {
+        int [][] dp=new int[m][n];
+        dp[0][0]=1;
+        for(int j=0;j<n;j++){
+            dp[0][j]=1;
         }
-        else if(s.length()>String.valueOf(Integer.MAX_VALUE).length()){
-            return false;
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
         }
-        else if(s.length()==String.valueOf(Integer.MAX_VALUE).length()&&s.compareTo(String.valueOf(Integer.MAX_VALUE))>0){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
-    public void dfs(String s,List<Integer> list){
-        if(s.length()==0|| tag){
-            if(list.size()>2){
-                tag=true;
-                for(int i=0;i<list.size();i++){
-                    result.add(list.get(i));
-                }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-        else{
-            if(list.size()<2){
-                for(int i=0;i<s.length();i++){
-                    String tmp=s.substring(0,i+1);
-                    if(isSplit(tmp)){
-                        int t=Integer.parseInt(tmp);
-                        list.add(t);
-                        dfs(s.substring(i+1),list);
-                        list.remove(list.size()-1);
-                    }
-
-                }
-            }
-            else{
-                for(int i=0;i<s.length();i++){
-                    String tmp=s.substring(0,i+1);
-                    if(isSplit(tmp)){
-                        int t=Integer.parseInt(tmp);
-                        if(t==list.get(list.size()-2)+list.get(list.size()-1)){
-                            list.add(t);
-                            dfs(s.substring(i+1),list);
-                            list.remove(list.size()-1);
-                        }
-                        else{
-                            if(i==s.length()-1)return;
-                        }
-                    }
-
-                }
-            }
-        }
-    }
-    public List<Integer> splitIntoFibonacci(String S) {
-        List<Integer> list=new ArrayList<>();
-        if(S.length()==1&&S.charAt(0)=='0'){
-            list.add(0);
-            return list;
-        }
-        else{
-            dfs(S, list);
-            if(tag){
-                return result;
-            }
-            else{
-                return new ArrayList<>();
-            }
-        }
-       
-    }
-
-    public static void main(String[] args) {
-       List<Integer> t= new Main().splitIntoFibonacci("214748364721474836422147483641");
-        System.out.println(t);
+        return dp[m-1][n-1];
     }
 
 }
