9ecc1170ac9e6bbd5d889f6fb46bd211873441ae
GeekVan <lychallengers@gmail.com>
2020-12-10 09:13:10 +0800

    1678. 设计 Goal 解析器

--- src/Main.java
+++ src/Main.java
@@ -6,54 +6,31 @@ import java.io.FileReader;
 import java.lang.*;
 import java.util.*;
 
-//860. 柠檬水找零
+//1678. 设计 Goal 解析器
 
 
 
 class Main {
-    public boolean lemonadeChange(int[] bills) {
-        HashMap<Integer,Integer> hashMap=new HashMap<>();
-        for(int i=0;i<bills.length;i++){
-            if(i==0){
-                if(bills[i]==5){
-                    hashMap.put(bills[i],1);
-                }
-                else{
-                    return false;
-                }
+    public String interpret(String command) {
+        StringBuffer sb=new StringBuffer();
+        int i=0;
+        while(i<command.length()){
+            char c=command.charAt(i);
+            if(c=='G'){
+                sb.append('G');
+                i=i+1;
             }
             else{
-                if(bills[i]==5){
-                    hashMap.put(5,hashMap.getOrDefault(5,0)+1);
+                if(command.charAt(i+1)==')'){
+                    sb.append('o');
+                    i=i+2;
                 }
-                else if(bills[i]==10){
-                    int t=hashMap.getOrDefault(5,0);
-                    if(t!=0){
-                        hashMap.put(5,t-1);
-                        hashMap.put(10,hashMap.getOrDefault(10,0)+1);
-                    }
-                    else{
-                        return false;
-                    }
-                }
-                else {
-                    int t1=hashMap.getOrDefault(10,0);
-                    int t2=hashMap.getOrDefault(5,0);
-                    if(t1!=0&&t2!=0){
-                        hashMap.put(10,t1-1);
-                        hashMap.put(5,t2-1);
-                        hashMap.put(20,hashMap.getOrDefault(20,0)+1);
-                    }
-                    else if(t1==0&&t2>=3){
-                        hashMap.put(5,t2-3);
-                        hashMap.put(20,hashMap.getOrDefault(20,0)+1);
-                    }
-                    else{
-                        return false;
-                    }
+                else{
+                    sb.append("al");
+                    i=i+4;
                 }
             }
         }
-        return true;
+        return sb.toString();
     }
 }
