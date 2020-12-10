17be7dc160356d21ffd834d8ca485d97dd09c75d
GeekVan <lychallengers@gmail.com>
2020-12-10 09:29:10 +0800

    1679. K 和数对的最大数目

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,48 @@ import java.io.FileReader;
 import java.lang.*;
 import java.util.*;
 
-//1678. 设计 Goal 解析器
+//1679. K 和数对的最大数目
 
 
 
 class Main {
-    public String interpret(String command) {
-        StringBuffer sb=new StringBuffer();
-        int i=0;
-        while(i<command.length()){
-            char c=command.charAt(i);
-            if(c=='G'){
-                sb.append('G');
-                i=i+1;
-            }
-            else{
-                if(command.charAt(i+1)==')'){
-                    sb.append('o');
-                    i=i+2;
+    public int maxOperations(int[] nums, int k) {
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        HashSet<Integer> hashSet=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            hashMap.put(nums[i],hashMap.getOrDefault(nums[i],0)+1);
+            hashSet.add(nums[i]);
+        }
+        int count=0;
+        for(Integer t:hashSet){
+            int target=k-t;
+            if(hashMap.containsKey(target)&&hashMap.containsKey(t)){
+                int t1=hashMap.getOrDefault(t,0);
+                int t2=hashMap.getOrDefault(target,0);
+                if(target==t){
+                    if(t1%2==0){
+                        hashMap.put(t,0);
+
+                    }
+                    else{
+                        hashMap.put(t,1);
+                    }
+                    count=count+t1/2;
                 }
                 else{
-                    sb.append("al");
-                    i=i+4;
+                    if(t1<t2){
+                        hashMap.put(t,0);
+                        hashMap.put(target,t2-t1);
+                        count=count+t1;
+                    }
+                    else{
+                        hashMap.put(target,0);
+                        hashMap.put(t,t1-t2);
+                        count=count+t2;
+                    }
                 }
             }
         }
-        return sb.toString();
+        return count;
     }
 }
