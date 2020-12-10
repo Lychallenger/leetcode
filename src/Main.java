38dd2e2eb770179bf95283e68b8aeb4c0761a66c
GeekVan <lychallengers@gmail.com>
2020-12-10 09:01:04 +0800

    860. 柠檬水找零

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,54 @@ import java.io.FileReader;
 import java.lang.*;
 import java.util.*;
 
-//62. 不同路径
+//860. 柠檬水找零
 
 
 
 class Main {
-    public int uniquePaths(int m, int n) {
-        int [][] dp=new int[m][n];
-        dp[0][0]=1;
-        for(int j=0;j<n;j++){
-            dp[0][j]=1;
-        }
-        for(int i=0;i<m;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+    public boolean lemonadeChange(int[] bills) {
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        for(int i=0;i<bills.length;i++){
+            if(i==0){
+                if(bills[i]==5){
+                    hashMap.put(bills[i],1);
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(bills[i]==5){
+                    hashMap.put(5,hashMap.getOrDefault(5,0)+1);
+                }
+                else if(bills[i]==10){
+                    int t=hashMap.getOrDefault(5,0);
+                    if(t!=0){
+                        hashMap.put(5,t-1);
+                        hashMap.put(10,hashMap.getOrDefault(10,0)+1);
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else {
+                    int t1=hashMap.getOrDefault(10,0);
+                    int t2=hashMap.getOrDefault(5,0);
+                    if(t1!=0&&t2!=0){
+                        hashMap.put(10,t1-1);
+                        hashMap.put(5,t2-1);
+                        hashMap.put(20,hashMap.getOrDefault(20,0)+1);
+                    }
+                    else if(t1==0&&t2>=3){
+                        hashMap.put(5,t2-3);
+                        hashMap.put(20,hashMap.getOrDefault(20,0)+1);
+                    }
+                    else{
+                        return false;
+                    }
+                }
             }
         }
-        return dp[m-1][n-1];
+        return true;
     }
-
 }
