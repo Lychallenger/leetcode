a3df6aa952139bc29758f129aecd78eb79a750aa
GeekVan <lychallengers@gmail.com>
2020-12-12 21:44:55 +0800

    376. 摆动序列

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,33 @@ import java.io.FileReader;
 import java.lang.*;
 import java.util.*;
 
-//649. Dota2 参议院
+//376. 摆动序列
 
 
 
 class Main {
-    public String predictPartyVictory(String senate) {
-        Queue<Integer> radiantQueue=new ArrayDeque<>();
-        Queue<Integer> direQueue=new ArrayDeque<>();
-        for(int i=0;i<senate.length();i++){
-            char ch=senate.charAt(i);
-            if(ch=='R'){
-                radiantQueue.offer(i);
+    public int wiggleMaxLength(int[] nums) {
+        if(nums.length<2){
+            return nums.length;
+        }
+        int [] up=new int[nums.length];
+        int [] down=new int[nums.length];
+        up[0]=1;
+        down[0]=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+                up[i]=Math.max(up[i-1],down[i-1]+1);
+                down[i]=down[i-1];
+            }
+            else if(nums[i]<nums[i-1]){
+                down[i]=Math.max(down[i-1],up[i-1]+1);
+                up[i]=up[i-1];
             }
             else{
-                direQueue.offer(i);
+                down[i]=down[i-1];
+                up[i]=up[i-1];
             }
         }
-        while(!radiantQueue.isEmpty()&&!direQueue.isEmpty()){
-            int radiantId=radiantQueue.poll();
-            int direId=direQueue.poll();
-            if(radiantId<direId){
-                radiantQueue.offer(radiantId+senate.length());
-            }
-            else{
-                direQueue.offer(direId+senate.length());
-            }
-        }
-        if(radiantQueue.isEmpty()){
-            return "Dire";
-        }
-        else{
-            return "Radiant";
-        }
+        return Math.max(up[nums.length-1],down[nums.length-1]);
     }
 }
