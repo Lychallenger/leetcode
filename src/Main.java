92c64afbb42809eff5caed08158eeefa48941098
GeekVan <lychallengers@gmail.com>
2020-12-13 10:36:59 +0800

    217. 存在重复元素

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,21 @@ import java.io.FileReader;
 import java.lang.*;
 import java.util.*;
 
-//376. 摆动序列
+//217. 存在重复元素
 
 
 
 class Main {
-    public int wiggleMaxLength(int[] nums) {
-        if(nums.length<2){
-            return nums.length;
-        }
-        int [] up=new int[nums.length];
-        int [] down=new int[nums.length];
-        up[0]=1;
-        down[0]=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>nums[i-1]){
-                up[i]=Math.max(up[i-1],down[i-1]+1);
-                down[i]=down[i-1];
-            }
-            else if(nums[i]<nums[i-1]){
-                down[i]=Math.max(down[i-1],up[i-1]+1);
-                up[i]=up[i-1];
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hashSet=new HashSet<>();
+        for(int t:nums){
+            if(hashSet.contains(t)){
+                return true;
             }
             else{
-                down[i]=down[i-1];
-                up[i]=up[i-1];
+                hashSet.add(t);
             }
         }
-        return Math.max(up[nums.length-1],down[nums.length-1]);
+        return false;
     }
 }
