68754faf2455edb8c0ca1a88802b964acf56b5ff
GeekVan <lychallengers@gmail.com>
2020-12-16 12:57:47 +0800

    290. 单词规律

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,46 @@
-
-import com.sun.org.apache.regexp.internal.RE;
-import org.omg.PortableInterceptor.INACTIVE;
-
-import java.awt.event.KeyEvent;
-import java.io.FileReader;
 import java.lang.*;
 import java.util.*;
 
-//738. 单调递增的数字
+//290. 单词规律
 
 
 
 class Main {
-    public int monotoneIncreasingDigits(int N) {
-        String s=String.valueOf(N);
-        if(s.length()==1){
-            return N;
+    public boolean wordPattern(String pattern, String s) {
+        String [] str=s.split("\\s+");
+        if(str.length!=pattern.length()){
+            return false;
         }
-        int i;
-        for(i=0;i<s.length()-1;i++){
-            if(s.charAt(i)<=s.charAt(i+1)){
-            }
-            else{
-                break;
-            }
-        }
-        if(i==s.length()-1){//全局单调递增
-            return N;
-        }
-        else{
-            String t=s.substring(0,i+1);
-            //查看是否尾数有相同的
-            int k;
-            for(k=t.length()-1;k>0;k--){
-                if(t.charAt(k)==t.charAt(k-1)){
+        HashMap<Character, String> chHashMap=new HashMap<>();
+        HashMap<String,Character> strHashMap=new HashMap<>();
+        for(int i=0;i<pattern.length();i++){
+            char ch=pattern.charAt(i);
+            if(chHashMap.containsKey(ch)){
+                String tmp=chHashMap.get(ch);
+                if(tmp.equals(str[i])){
+                    continue;
                 }
                 else{
-                    break;
+                    return false;
                 }
-            }
-            int m;
-            String sb;
-            StringBuilder x= new StringBuilder();
-            if(k!=t.length()-1){
-                sb=t.substring(0,k+1);
-                k=k+1;
-                for(int n=0;n<t.length()-k;n++){
-                    x.append("9");
-                }
-                sb=String.valueOf(Integer.parseInt(sb)-1);
-                String new_t=sb+x;
-                m=Integer.parseInt(new_t);
             }
             else{
-                m=Integer.parseInt(t);
-                m=m-1;
+                if(strHashMap.containsKey(str[i])){
+                    char tp=strHashMap.get(str[i]);
+                    if(tp!=ch){
+                        return false;
+                    }
+                    else{
+                        continue;
+                    }
+                }
+                else{
+                    strHashMap.put(str[i],ch);
+                    chHashMap.put(ch,str[i]);
+                }
             }
-
-            StringBuilder other= new StringBuilder();
-            for(int j=0;j<s.length()-i-1;j++){
-                other.append("9");
-            }
-            String result=String.valueOf(m)+other;
-            return Integer.parseInt(result);
         }
+        return true;
 
-    }
-
-    public static void main(String[] args) {
-       int t= new Main().monotoneIncreasingDigits(332);
-        System.out.println(t);
     }
 }
