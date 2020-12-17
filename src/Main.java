c7b9c7b12baab537f15b1b60850634708645fa41
GeekVan <lychallengers@gmail.com>
2020-12-17 11:05:23 +0800

    714. 买卖股票的最佳时机含手续费

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,21 @@
 import java.lang.*;
 import java.util.*;
 
-//290. 单词规律
+//714. 买卖股票的最佳时机含手续费
 
 
 
 class Main {
-    public boolean wordPattern(String pattern, String s) {
-        String [] str=s.split("\\s+");
-        if(str.length!=pattern.length()){
-            return false;
-        }
-        HashMap<Character, String> chHashMap=new HashMap<>();
-        HashMap<String,Character> strHashMap=new HashMap<>();
-        for(int i=0;i<pattern.length();i++){
-            char ch=pattern.charAt(i);
-            if(chHashMap.containsKey(ch)){
-                String tmp=chHashMap.get(ch);
-                if(tmp.equals(str[i])){
-                    continue;
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                if(strHashMap.containsKey(str[i])){
-                    char tp=strHashMap.get(str[i]);
-                    if(tp!=ch){
-                        return false;
-                    }
-                    else{
-                        continue;
-                    }
-                }
-                else{
-                    strHashMap.put(str[i],ch);
-                    chHashMap.put(ch,str[i]);
-                }
-            }
-        }
-        return true;
 
+
+    public int maxProfit(int[] prices, int fee) {
+        int [][]dp=new int[prices.length][2];
+        dp[0][0]=0;
+        dp[0][1]=-prices[0];
+        for(int i=1;i<prices.length;i++){
+            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]+ prices[i]-fee);
+            dp[i][1]=Math.max(dp[i-1][1],dp[i-1][0]-prices[i]);
+        }
+        return dp[prices.length-1][0];
     }
 }
