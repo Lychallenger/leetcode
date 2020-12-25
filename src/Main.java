e6326f9efa5b8ebed1a9349487d08ca98df62287
GeekVan <lychallengers@gmail.com>
2020-12-25 09:09:53 +0800

    455. 分发饼干

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,26 @@
 import java.lang.*;
 import java.util.*;
 
-//135. 分发糖果
+//455. 分发饼干
 
 
 
 class Main {
 
-
-    public int candy(int[] ratings) {
-        int []dp=new int[ratings.length];
-        dp[0]=1;
-        for(int i=0;i<ratings.length-1;i++){
-            if(ratings[i]<ratings[i+1]){
-                dp[i+1]=dp[i]+1;
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ct=0;
+        int count=0;
+        for(int i=0;i<s.length;i++){
+            if(ct<g.length&&s[i]>=g[ct]){
+                count++;
+                ct++;
             }
-            else{
-                dp[i+1]=1;
+            else {
+                continue;
             }
         }
-        int sum=0;
-        for(int i=ratings.length-1;i>0;i--){
-            if(ratings[i-1]>ratings[i]){
-                dp[i-1]=Math.max(dp[i-1],dp[i]+1);
-            }
-            sum=sum+dp[i-1];
-        }
-        sum=sum+dp[dp.length-1];
-        return sum;
+        return count;
     }
 }
