9288718fc4a1ee4b6899bb76e607d916c2014a9e
GeekVan <lychallengers@gmail.com>
2020-12-29 09:18:27 +0800

    330. 按要求补齐数组

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,25 @@
 import java.lang.*;
 import java.util.*;
 
-//188. 买卖股票的最佳时机 IV
+//330. 按要求补齐数组
 
 
 
 class Main {
 
-    public int maxProfit(int k, int[] prices) {
-
-        if(k>=prices.length/2){
-            int sum=0;
-            for(int i=1;i<prices.length;i++){
-                if(prices[i]>prices[i-1]){
-                    sum=sum+prices[i]-prices[i-1];
-                }
-            }
-            return sum;
-        }
-        int [][][]dp=new int[prices.length+1][k+1][2];
-        //初始值
-        for(int i=0;i<prices.length+1;i++){
-            dp[i][0][0]=0;
-            dp[i][0][1]=Integer.MIN_VALUE;
-        }
-        for(int i=0;i<=k;i++){
-            dp[0][i][0]=0;
-            dp[0][i][1]=-prices[0];
-        }
-        for(int i=1;i<=prices.length;i++){
-            for(int j=1;j<=k;j++){
-                dp[i][j][0]=Math.max(dp[i-1][j][0],dp[i-1][j][1]+prices[i-1]);
-                dp[i][j][1]=Math.max(dp[i-1][j][1],dp[i-1][j-1][0]-prices[i-1]);
+    public int minPatches(int[] nums, int n) {
+        int patches = 0;
+        long x = 1;
+        int length = nums.length, index = 0;
+        while (x <= n) {
+            if (index < length && nums[index] <= x) {
+                x += nums[index];
+                index++;
+            } else {
+                x *= 2;
+                patches++;
             }
         }
-        return dp[prices.length][k][0];
+        return patches;
     }
 }
