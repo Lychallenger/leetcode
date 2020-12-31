46700f98acc1e6eb8f5a648e121c93b8279b5234
GeekVan <lychallengers@gmail.com>
2020-12-31 09:31:54 +0800

    435. 无重叠区间

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ class Main {
                 return o1[1]-o2[1];
             }
         });
-        int count=0;
+        int count=1;
         int end=intervals[0][1];
         for(int i=0;i<intervals.length;i++){
             if(intervals[i][0]>=end){
