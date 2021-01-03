99dda8d2b2cb608fbe4f771eaea58f92249cd363
GeekVan <lychallengers@gmail.com>
2021-01-03 12:14:38 +0800

    86. 分隔链表

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,37 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//435. 无重叠区间
+//86. 分隔链表
 
 
 
 class Main {
 
-    public int eraseOverlapIntervals(int[][] intervals) {
-        if(intervals.length==0){
-            return 0;
-        }
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1]-o2[1];
+    public ListNode partition(ListNode head, int x) {
+            ListNode p1=new ListNode(-1);
+            ListNode p2=p1;
+            p1.next=head;
+            ListNode pre=p1;
+            while(head!=null&&head.val<x){
+                p1=p1.next;
+                head=head.next;
+                pre=pre.next;
             }
-        });
-        int count=1;
-        int end=intervals[0][1];
-        for(int i=0;i<intervals.length;i++){
-            if(intervals[i][0]>=end){
-                end=intervals[i][1];
-                count++;
+            while (head!=null){
+                if(head.val<x){
+                    ListNode t1=head;
+                    ListNode tmp=head.next;
+                    t1.next=p1.next;
+                    p1.next=t1;
+                    p1=p1.next;
+                    head=tmp;
+                    pre.next=tmp;
+                }
+                else{
+                    pre=head;
+                    head=head.next;
+                }
             }
-        }
-        return intervals.length-count;
+            return p2.next;
     }
 }
