68538c47ee6eb23a2f6c57a85e8b576681c08445
GeekVan <lychallengers@gmail.com>
2021-01-04 09:29:58 +0800

    509. 斐波那契数

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,18 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//86. 分隔链表
+//509. 斐波那契数
 
 
 
 class Main {
 
-    public ListNode partition(ListNode head, int x) {
-            ListNode p1=new ListNode(-1);
-            ListNode p2=p1;
-            p1.next=head;
-            ListNode pre=p1;
-            while(head!=null&&head.val<x){
-                p1=p1.next;
-                head=head.next;
-                pre=pre.next;
-            }
-            while (head!=null){
-                if(head.val<x){
-                    ListNode t1=head;
-                    ListNode tmp=head.next;
-                    t1.next=p1.next;
-                    p1.next=t1;
-                    p1=p1.next;
-                    head=tmp;
-                    pre.next=tmp;
-                }
-                else{
-                    pre=head;
-                    head=head.next;
-                }
-            }
-            return p2.next;
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
     }
 }
