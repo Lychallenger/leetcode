7564f0843912d20d0ac3f028b95d2b02fc56de3c
GeekVan <lychallengers@gmail.com>
2021-01-10 12:05:29 +0800

    228. 汇总区间

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,45 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//547. 省份数量
+//228. 汇总区间
 
 
 
 class Main {
 
-    public  void dfs(int i,int [][]isConnected,boolean[]visit){
-        visit[i]=true;
-        for(int j=0;j<visit.length;j++){
-            if(isConnected[i][j]==1&&visit[j]==false){
-                dfs(j,isConnected,visit);
-            }
+    public List<String> summaryRanges(int[] nums) {
+
+        List<String> result=new ArrayList<>();
+        if(nums.length==0){
+            return result;
         }
-    }
-    public int findCircleNum(int[][] isConnected) {
-        boolean [] visit=new boolean[isConnected.length];
-        int count=0;
-        for(int i=0;i<visit.length;i++){
-            if(visit[i]==false){
-                count++;
-                dfs(i,isConnected,visit);
+        int i=1;
+        int begin=0;
+        int end=0;
+        while(i<nums.length){
+            if(nums[i]-nums[end]==1){
+                end=i;
             }
+            else{
+                //write result
+                if(begin==end){
+                    result.add(String.valueOf(nums[begin]));
+                }
+                else{
+                    result.add(String.valueOf(nums[begin])+"->"+String.valueOf(nums[end]));
+
+                }
+                begin=i;
+                end=i;
+            }
+            i++;
         }
-        return count;
+        if(begin==end){
+            result.add(String.valueOf(nums[begin]));
+        }
+        else{
+            result.add(String.valueOf(nums[begin])+"->"+String.valueOf(nums[end]));
+        }
+        return result;
     }
 }
