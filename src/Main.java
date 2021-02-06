b5244b9f4009306e0c94b36a2a320f6e27e59d90
GeekVan <lychallengers@gmail.com>
2021-02-06 16:09:12 +0800

    1423. 可获得的最大点数

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,32 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//1208. 尽可能使字符串相等
+//1423. 可获得的最大点数
 
 
 
 class Main {
 
-    public int equalSubstring(String s, String t, int maxCost) {
-        int left=0;
-        int right=0;
-        int costSum=0;
-        int max_len=0;
-        while(right<s.length()){
-            if(costSum<=maxCost){
-                max_len=Math.max(max_len,right-left);
-                costSum=costSum+Math.abs(s.charAt(right)-t.charAt(right));
-                right++;
-            }
-            else{
-                max_len=Math.max(max_len,right-left-1);
-                costSum=costSum-Math.abs(s.charAt(left)-t.charAt(left));
-                left++;
-            }
+    public int maxScore(int[] cardPoints, int k) {
+        int tmp_sum=0;
+        int total=0;
+        int min_sum;
+        int window_size=cardPoints.length-k;
+        for(int i=0;i<window_size;i++){
+            tmp_sum=tmp_sum+cardPoints[i];
+            total=total+cardPoints[i];
         }
-        if(costSum<=maxCost){
-            max_len=Math.max(max_len,right-left);
+        min_sum=tmp_sum;
+        //slide window
+        for(int i=window_size;i<cardPoints.length;i++){
+            //remove left;
+            tmp_sum=tmp_sum-cardPoints[i-window_size];
+            //add right;
+            tmp_sum=tmp_sum+cardPoints[i];
+            min_sum=Math.min(min_sum,tmp_sum);
+            total=total+cardPoints[i];
         }
-        return max_len;
+        return total-min_sum;
+
     }
 }
