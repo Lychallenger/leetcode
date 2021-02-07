c6aec0bf81366784df8231672e3cf8dda43dc900
GeekVan <lychallengers@gmail.com>
2021-02-07 14:34:12 +0800

    665. 非递减数列

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,28 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//888. 公平的糖果棒交换
+//665. 非递减数列
 
 
 
 class Main {
 
-    public int[] fairCandySwap(int[] A, int[] B) {
-        int sum1=0;
-        int sum2=0;
-        //set
-        Set<Integer> set=new LinkedHashSet<>();
-        for(int i=0;i<B.length;i++){
-            sum2=sum2+B[i];
-            set.add(B[i]);
-        }
-        for(int i=0;i<A.length;i++){
-            sum1=sum1+A[i];
-        }
-        int mean=(sum1+sum2)/2;
-        int expectValue=sum1-mean;
-        for(int i=0;i<A.length;i++){
-            if(set.contains(A[i]-expectValue)){
-                return new int[]{A[i],A[i]-expectValue};
+    //4 2 1
+    // 4 2 3
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length, cnt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int x = nums[i], y = nums[i + 1];
+            if (x > y) {
+                cnt++;
+                if (cnt > 1) {
+                    return false;
+                }
+                if (i > 0 && y < nums[i - 1]) {
+                    nums[i + 1] = x;
+                }
             }
         }
-        return new int[]{};
+        return true;
     }
 }
