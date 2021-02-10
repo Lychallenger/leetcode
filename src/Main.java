50cc805bf09df1dbed55039be6ba54cb4f28f0ef
GeekVan <lychallengers@gmail.com>
2021-02-10 09:39:18 +0800

    567. 字符串的排列

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,37 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//978. 最长湍流子数组
+//567. 字符串的排列
 
 
 
 class Main {
 
-    public int maxTurbulenceSize(int[] arr) {
-        if(arr.length<2){
-            return arr.length;
+    public boolean checkInclusion(String s1, String s2) {
+        int[] hashmap=new int[26];
+        for(int i=0;i<s1.length();i++){
+            hashmap[s1.charAt(i)-'a']++;
         }
-        int max_len=1;
-        int tmp_len=1;
-        for(int i=0;i<arr.length-1;i++){
-            if(i%2==0){
-                if(arr[i]>arr[i+1]){
-                    tmp_len++;
-                }
-                else{
-                    tmp_len=1;
-                }
+        int left=0;
+        int right=0;
+        hashmap[s2.charAt(right)-'a']--;
+        while(right<s2.length()){
+            if(hashmap[s2.charAt(right)-'a']<0){
+                hashmap[s2.charAt(left)-'a']++;
+                left++;
             }
             else{
-                if(arr[i]<arr[i+1]){
-                    tmp_len++;
+                if(right-left+1==s1.length()){
+                    return true;
                 }
-                else{
-                    tmp_len=1;
+                right++;
+                if(right>=s2.length()){
+                    break;
                 }
+                hashmap[s2.charAt(right)-'a']--;
             }
-            max_len=Math.max(max_len,tmp_len);
+
         }
-        int len_tmp=1;
-        for(int i=0;i<arr.length-1;i++){
-            if(i%2==1){
-                if(arr[i]>arr[i+1]){
-                    len_tmp++;
-                }
-                else{
-                    len_tmp=1;
-                }
-            }
-            else{
-                if(arr[i]<arr[i+1]){
-                    len_tmp++;
-                }
-                else{
-                    len_tmp=1;
-                }
-            }
-            max_len=Math.max(max_len,len_tmp);
-        }
-        return max_len;
+        return false;
     }
 }
