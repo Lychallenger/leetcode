4a380055c189da34b99567860fdaaa632bdfe4af
GeekVan <lychallengers@gmail.com>
2021-02-12 09:27:31 +0800

    119. 杨辉三角 II

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,32 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//703. 数据流中的第 K 大元素
+//119. 杨辉三角 II
 
 
 
 class Main {
-    int k;
-    PriorityQueue<Integer> priorityQueue;
-    public Main(int k, int[] nums) {
-        this.k=k;
-        this.priorityQueue=new PriorityQueue<>();
-        //init data
-        for(int i=0;i<(k>nums.length?nums.length:k);i++){
-            priorityQueue.add(nums[i]);
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list=new LinkedList<>();
+        list.add(0,1);
+        if(rowIndex==0){
+            return list;
         }
-        for(int i=k;i<nums.length;i++){
-            if(nums[i]>priorityQueue.peek()){
-                priorityQueue.poll();
-                priorityQueue.add(nums[i]);
-            }
-        }
-    }
-
-    public int add(int val) {
-        if(priorityQueue.size()<k){
-            priorityQueue.add(val);
+        else if(rowIndex==1){
+            list.add(0,1);
+            return list;
         }
         else{
-            if(val>priorityQueue.peek()){
-                priorityQueue.poll();
-                priorityQueue.add(val);
+            list.add(0,1);
+            for(int i=1;i<rowIndex;i++){
+                //loop add;
+                for(int j=0;j<list.size()-1;j++){
+                    list.set(j,list.get(j)+list.get(j+1));
+                }
+                //add 1
+                list.add(0,1);
             }
+            return list;
         }
-        return priorityQueue.peek();
     }
 }
