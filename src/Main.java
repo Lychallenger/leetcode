6931629034af580217711fef937977a8c38ebc7e
GeekVan <lychallengers@gmail.com>
2021-02-16 08:53:48 +0800

    561. 数组拆分 I

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,16 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//485. 最大连续1的个数
+//561. 数组拆分 I
 
 
 class Main {
-    public int findMaxConsecutiveOnes(int[] nums) {
-        int count=0;
-        int max_count=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==1){
-                count++;
-            }
-            else{
-                max_count=Math.max(max_count,count);
-                count=0;
-            }
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int ret=0;
+        for(int i=0;i<nums.length;i=i+2){
+            ret=ret+nums[i];
         }
-        max_count=Math.max(max_count,count);
-        return max_count;
+        return ret;
     }
 }
