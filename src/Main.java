ead95f9daf753c9ddd33fe579c96528ea2d4720c
GeekVan <lychallengers@gmail.com>
2021-02-20 09:06:49 +0800

    697. 数组的度

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,38 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//1004. 最大连续1的个数 III
+//697. 数组的度
 
-
+class arr{
+    int left;
+    int right;
+    int count;
+    arr(int left){
+        this.left=left;
+        this.right=left;
+        this.count=1;
+    }
+}
 class Main {
-    public int longestOnes(int[] A, int K) {
-        int l = 0, r = 0, res = 0;
-        while (r < A.length) {
-            if (A[r] == 0) {
-                if (K == 0) {
-                    while (A[l] == 1) l++;
-                    l++;
-                } else {
-                    K--;
-                }
+    public int findShortestSubArray(int[] nums) {
+        HashMap<Integer,arr> hashMap=new HashMap<>();
+        int max_count=0;
+        for(int i=0;i<nums.length;i++){
+            if(!hashMap.containsKey(nums[i])){
+                hashMap.put(nums[i],new arr(i));
             }
-            res = Math.max(res, ++r - l);
+            else{
+                hashMap.get(nums[i]).right=i;
+                hashMap.get(nums[i]).count++;
+            }
+            max_count=Math.max(hashMap.get(nums[i]).count,max_count);
         }
-        return res;
+        int max_len=Integer.MAX_VALUE;
+        for(Integer keys:hashMap.keySet()){
+            if(hashMap.get(keys).count==max_count){
+                max_len=Math.min(max_len,hashMap.get(keys).right-hashMap.get(keys).left+1);
+            }
+        }
+        return max_len;
     }
 }
