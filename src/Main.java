4c45d1f6ce55b715618db1f42d3a6e4c66c7625e
GeekVan <lychallengers@gmail.com>
2021-03-04 17:07:36 +0800

    354. 俄罗斯套娃信封问题

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,35 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//338. 比特位计数
+//354. 俄罗斯套娃信封问题
 
 class Main {
-    public int[] countBits(int num) {
-        int [] re=new int[num+1];
-        re[0]=0;
-        if(num==0)return re;
-        else if(num==1){
-            re[1]=1;
-            return re;
+    public int maxEnvelopes(int[][] envelopes) {
+        if(envelopes.length==0){
+            return 0;
         }
-        else{
-            re[1]=1;
-            for(int i=2;i<re.length;i++){
-                if(i%2==0){
-                    re[i]=re[i/2];
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]!=o2[0]){
+                    return o1[0]-o2[0];
                 }
                 else{
-                    re[i]=re[i-1]+1;
+                    return o2[1]-o1[1];
                 }
             }
+        });
+        int max=1;
+        int []dp=new int[envelopes.length];
+        Arrays.fill(dp,1);
+        for(int i=1;i<envelopes.length;i++){
+            for(int j=0;j<i;j++){
+                if(envelopes[i][1]>envelopes[j][1]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            max=Math.max(max,dp[i]);
         }
-        return re;
-
+        return max;
     }
 }
