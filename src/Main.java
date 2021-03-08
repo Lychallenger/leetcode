5cd64e0abfadc77714a17b9742d42f01923bbd1f
GeekVan <lychallengers@gmail.com>
2021-03-08 09:02:02 +0800

    132. 分割回文串 II

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,45 @@ import sun.nio.cs.ext.MacHebrew;
 import java.lang.*;
 import java.util.*;
 
-//131. 分割回文串
+//132. 分割回文串 II
 
 class Main {
-    public void dfs(String s,int off,boolean[][]dp,List<String> tmp,List<List<String>> re){
-        if(off==s.length()){
-            re.add(new ArrayList<>(tmp));
-            return;
-        }
-        for(int i=off;i<s.length();i++){
-            if(dp[off][i]){
-                tmp.add(s.substring(off,i+1));
-                dfs(s,i+1,dp,tmp,re);
-                tmp.remove(tmp.size()-1);
-            }
-        }
-    }
-    public List<List<String>> partition(String s) {
-        boolean [][] dp=new boolean[s.length()][s.length()];
-        for(int step=0;step<s.length();step++){
-            for(int i=0;i+step<s.length();i++){
-                if(step==0){
-                    dp[i][i+step]=true;
+    public int minCut(String s) {
+        boolean [][] isRe=new boolean[s.length()][s.length()];
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j+i<s.length();j++){
+                if(i==0){
+                    isRe[j][j+i]=true;
                 }
-                else if(step==1){
-                    if(s.charAt(i)==s.charAt(i+step)){
-                        dp[i][i+step]=true;
+                else if(i==1){
+                    if(s.charAt(j)==s.charAt(j+i)){
+                        isRe[j][j+i]=true;
                     }
                     else{
-                        dp[i][i+step]=false;
+                        isRe[j][j+i]=false;
                     }
                 }
                 else{
-                    if(s.charAt(i)==s.charAt(i+step) && dp[i+1][i+step-1]){
-                        dp[i][i+step]=true;
+                    if(isRe[j+1][j+i-1]&&s.charAt(j)==s.charAt(j+i)){
+                        isRe[j][j+i]=true;
                     }
                     else{
-                        dp[i][i+step]=false;
+                        isRe[j][j+i]=false;
                     }
                 }
             }
         }
-        List<String> tmp=new ArrayList<>();
-        List<List<String>> re=new ArrayList<>();
-        dfs(s,0,dp,tmp,re);
-        return re;
 
+        int []dp=new int[s.length()+1];
+        dp[0]=0;
+        for(int i=0;i<s.length();i++){
+            dp[i+1]=i+1;
+            for(int j=i;j>=0;j--){
+                if(isRe[j][i]){
+                    dp[i+1]=Math.min(dp[i+1],dp[j]+1);
+                }
+            }
+        }
+        return dp[s.length()];
     }
 }
