7c934d3804ea4a3a3f2728f1e19751ccd9e47023
GeekVan <lychallengers@gmail.com>
2021-05-20 10:36:15 +0800

    692. 前K个高频单词

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,42 @@
-import sun.nio.cs.ext.MacHebrew;
 
 import java.lang.*;
 import java.util.*;
 
-//82. 删除排序链表中的重复元素 II
+//692. 前K个高频单词
 
 class Main {
-    public ListNode deleteDuplicates(ListNode head) {
-        ListNode ret=new ListNode(-1);
-        ListNode result=ret;
-        if(head==null){
-            return null;
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> hashMap=new HashMap<>();
+        for(int i=0;i<words.length;i++){
+            hashMap.put(words[i],hashMap.getOrDefault(words[i],0)+1);
         }
-        ret.next=head;
-        while(head.next!=null){
-            if(head.val==head.next.val){
-                ret.next=null;
+        PriorityQueue<Map.Entry<String, Integer>> priorityQueue = new PriorityQueue<>(
+                (o1, o2) ->{
+                    if(o1.getValue().equals(o2.getValue())){
+                        return o1.getKey().compareTo(o2.getKey());
+                    }
+                    else{
+                        return o1.getValue()-o2.getValue();
+                    }
+                } );
+        for(Map.Entry<String,Integer> entry:hashMap.entrySet()){
+            if(priorityQueue.size()<k){
+                priorityQueue.add(entry);
             }
             else{
-                if(ret.next!=null){
-                    ret=ret.next;
+                if(entry.getValue()>priorityQueue.peek().getValue()||
+                        (entry.getValue().equals(priorityQueue.peek().getValue())
+                                &&entry.getKey().compareTo(priorityQueue.peek().getKey())<0)){
+                    priorityQueue.poll();
+                    priorityQueue.add(entry);
                 }
-                ret.next=head.next;
             }
-            head=head.next;
         }
-        return result.next;
+        List<String> list=new ArrayList<>();
+        for(int i=0;i<k;i++){
+            list.add(0,priorityQueue.poll().getKey());
+        }
+        return list;
     }
+
 }
