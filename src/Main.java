b35c03480796d7463076a9acb3e5ca84d4c7c415
GeekVan <lychallengers@gmail.com>
2021-07-13 16:53:57 +0800

    1929. 数组串联

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,15 @@
 import java.lang.*;
 import java.util.*;
 
-//930. 和相同的二元子数组
+//1929. 数组串联
 
 class Main {
-    public int numSubArraysWithSum(int[] nums, int goal) {
-        if(nums.length==0){
-            return 0;
-        }
-        int sum=0;
-        int ret=0;
+    public int[] getConcatenation(int[] nums) {
+        int [] ans=new int[nums.length*2];
         for(int i=0;i<nums.length;i++){
-            int t=nums[i];
-            nums[i]=0;//init set to 0;
-            nums[sum]=nums[sum]+1;
-            sum=sum+t;
-            if(sum-goal>=0){
-                if(sum-goal<nums.length&&sum-goal<=i)
-                    ret=ret+nums[sum-goal];
-            }
-
+            ans[i]=nums[i];
+            ans[i+ nums.length]=nums[i];
         }
-        return ret;
-
+        return ans;
     }
 }
