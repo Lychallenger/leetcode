c385c1d5f61d9bd9985abf1ff8b2e7b31c08822d
GeekVan <lychallengers@gmail.com>
2021-07-15 10:36:57 +0800

    1846. 减小和重新排列数组后的最大元素

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,17 @@
 import java.lang.*;
 import java.util.*;
 
-//1930. 长度为 3 的不同回文子序列
+//1846. 减小和重新排列数组后的最大元素
 
 class Main {
-    public int countPalindromicSubsequence(String s) {
-        int [] first=new int[26];
-        int [] last =new int[26];
-        for(int i=0;i<26;i++){
-            first[i]=-1;
-            last[i]=-1;
-        }
-        for(int i=0;i<s.length();i++){
-            if(first[s.charAt(i)-'a']==-1){
-                first[s.charAt(i)-'a']=i;
-            }
-            else{
-                last[s.charAt(i)-'a']=i;
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0]=1;//set to 1
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-arr[i-1]>1){
+                arr[i]=arr[i-1]+1;
             }
         }
-        int count=0;
-        for(int i=0;i<26;i++){
-            if(first[i]!=-1&&last[i]!=-1){
-                HashSet<Character> hashSet=new HashSet<>();
-                for(int j=first[i]+1;j<last[i];j++){
-                    hashSet.add(s.charAt(j));
-                }
-                count=count+hashSet.size();
-            }
-        }
-        return count;
-    }
-
-    public static void main(String[] args) {
-        String s="abca";
-        int a=s.charAt(1)-'a';
-        System.out.println(a);;
+        return arr[arr.length-1];
     }
 }
