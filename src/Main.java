e409b07f731454c623143a75ef8ee8df38042d33
ly <37396230+Lychallenger@users.noreply.github.com>
2021-08-10 11:09:48 +0800

    topk问题

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@
 import java.lang.*;
 import java.util.*;
 
-//1846. 减小和重新排列数组后的最大元素
+//1642. 可以到达的最远建筑
 
 class Main {
-    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        Arrays.sort(arr);
-        arr[0]=1;//set to 1
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]-arr[i-1]>1){
-                arr[i]=arr[i-1]+1;
+   public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> priorityQueue =new PriorityQueue<>((o1,o2)->o1-o2);
+        for(int i=0;i<heights.length-1;i++){
+            if(heights[i]<heights[i+1]){
+                priorityQueue.add(heights[i+1]-heights[i]);
+                if(priorityQueue.size()>ladders){
+                    bricks=bricks-priorityQueue.poll();
+                }
+                if(bricks<0){
+                    return i;
+                }
             }
         }
-        return arr[arr.length-1];
+        return heights.length-1;
     }
 }
